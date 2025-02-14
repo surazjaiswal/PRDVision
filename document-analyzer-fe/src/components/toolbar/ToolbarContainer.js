d645521b53d75802d3ac284d61095acc304b95eb
Shubham Gupta <sgupta@docquity.com>
2025-02-14 16:38:49 +0530

    Zoom in out added.

--- document-analyzer-fe/src/components/toolbar/ToolbarContainer.js
+++ document-analyzer-fe/src/components/toolbar/ToolbarContainer.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Plus, Minus, Share2, Download, ChevronDown } from "lucide-react";
 import './ToolbarContainer.css'
+import ZoomInIcon from '../../assets/ic_zoom_in.svg';
+import ZoomOutIcon from '../../assets/ic_zoom_out.svg';
 
-const Toolbar = ({ onSelectOption, availableKeys }) => {
+const Toolbar = ({ onSelectOption, availableKeys, zoomIn, zoomOut, zoomPercentage, showZoomControls }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(availableKeys[0]);
 
@@ -20,6 +22,7 @@ const Toolbar = ({ onSelectOption, availableKeys }) => {
             <div></div><div></div><div></div><div></div>
             </div>
         </button>
+
         <div className="dropdown-container">
             <button className="dropdown-button" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
             {selectedOption} <ChevronDown className="icon" />
@@ -34,17 +37,27 @@ const Toolbar = ({ onSelectOption, availableKeys }) => {
             </ul>
             )}
         </div>
-
       </div>
 
-      <div class="toolbar-element">
-        <button className="share-button">
-            <Share2 className="icon" /> Share
-        </button>
-        <button className="export-button">
-            <Download className="icon" /> Export
-        </button>
-      </div>
+      {showZoomControls && 
+         <div class="zoom-controllers">
+         <button onClick={zoomOut} className="zoom-btn" disabled={zoomPercentage <= 40}>
+           <img src={ZoomOutIcon} alt="Zoom Out" className="zoom-icon" />
+         </button>
+ 
+         <span className="zoom-counter">{zoomPercentage}%</span>
+ 
+         <button onClick={zoomIn} className="zoom-btn" disabled={zoomPercentage >= 200}>
+           <img src={ZoomInIcon} alt="Zoom In" className="zoom-icon" />
+         </button>
+         <button className="share-button hidden">
+             <Share2 className="icon"/> Share
+         </button>
+         <button className="export-button hidden">
+             <Download className="icon" /> Export
+         </button>
+       </div>
+      }
     </div>
   );
 };
